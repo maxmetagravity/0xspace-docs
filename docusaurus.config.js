69c234f7c0eb4b0f96da1ba7c53eb9b7c4d7784d
maxmetagravity <iamliqiang@gmail.com>
2022-07-28 13:01:12 +0800

    4.chart

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/maxmetagravity/0xspace-docs/',
+            'https://github.com/maxmetagravity/0xspace-docs/blob/master/',
         },
         blog: {
           showReadingTime: true,
