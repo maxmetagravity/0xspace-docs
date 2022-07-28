844379d26dbe4e7d61b755b92a3530dc54e5dea0
maxmetagravity <iamliqiang@gmail.com>
2022-07-28 14:40:31 +0800

    creat and join

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/introduction/welcome">
+            0xSPACE Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
